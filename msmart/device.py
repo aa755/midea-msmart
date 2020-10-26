8c288e9155a182ab9de68bf25bb020c4e8e81f7a
Abhishek Anand (optiplex) <abhishek.anand.iitg@gmail.com>
2020-10-25 21:48:44 -0700

    more debugging

--- msmart/device.py
+++ msmart/device.py
@@ -264,7 +264,7 @@ class air_conditioning_device(device):
                     self.tempcontrol_usermode=self._operational_mode
             self.tempcontrol_overriden_fan=newtemp_control_override
             newmode=air_conditioning_device.operational_mode_enum.fan_only if self.tempcontrol_overriden_fan else self._operational_mode
-            print(f'oldmode:{ac_mode}, newmode:{newmode}')
+            print(f'oldmode:{ac_mode}, newmode:{newmode}, usermode:{self.tempcontrol_usermode}, target:{self._target_temperature}, curtemp: {self._indoor_temperature}, fan_only_overridden: {self.tempcontrol_overriden_fan}')
             if (newmode!=ac_mode):
                 self._defer_update = True
                 self.apply()
